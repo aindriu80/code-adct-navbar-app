18789651c8e2c72950123a94e12dc6d2e77b862a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-24 11:57:26 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>React Navbar App</h2>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
